27ed909202f3682d6753ee9bae0aa504f45b381b
yuaneg <1534915553@qq.com>
2015-11-06 23:49:30 +0800

    yuan

--- SpringJDBC/src/demo/dao/DaoDemo.java
+++ SpringJDBC/src/demo/dao/DaoDemo.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 /**
- * @description 测试用的数据库是 cms 官网的数据库
+ * @description 测试用的数据库是 普惠家 mysql 数据库
  * @author yuaneg
  * 2015年11月1日下午9:01:32
  */
@@ -22,7 +22,7 @@ public class DaoDemo extends BaseDao{
 	 */
 	@Deprecated
 	public int queryForInt(){
-		String sql="select count(*) from px_c_message";
+		String sql="select count(*) from sys_user";
 		//第一种方式
 		int t=this.jdbcTemplate.queryForInt(sql);
 		return t;
@@ -34,23 +34,23 @@ public class DaoDemo extends BaseDao{
 	 * 2015年11月1日下午10:06:13
 	 */
 	public Object queryForObject(){
-		String sql="select count(*) from px_c_message";
+		String sql="select count(*) from sys_user";
 		// 第二个参数 只能用 integer.class 等包装类的.class  用int.class 会出错误   
 		//queryForObject(String sql, Class<T> requiredType)
 		//第一个参数 是 sql 第二个参数是要查询 的 返回值的类型  如果写 3 个参数 第三个 参数是 ... (list) 前面 ? 的赋值
 		int t = this.jdbcTemplate.queryForObject(sql, Integer.class);
-		sql="select message_name from px_c_message where message_id=?";
+		sql="select t.real_name from sys_user t where id=?";
 		String str = this.jdbcTemplate.queryForObject(sql,String.class,190L);
 		
 		//只有以上两种方法    的参数类型是 String sql, Class<T> requiredType, Object... args
 		
-		sql="select message_name from px_c_message where message_id=?";
+		sql="select t.real_name from sys_user t where id=?";
 		//第三个参数 指定 Object 里面的整数类型 可以不写
 		str=this.jdbcTemplate.queryForObject(sql,new Object[]{190},new int[]{1},String.class);
 		str=this.jdbcTemplate.queryForObject(sql,new Object[]{190},String.class);
 		
 		//采用 RowMapper 接口获得自定义对象
-		sql="select message_name,message_id from px_c_message where message_id=?";
+		sql="select t.real_name,t.id from sys_user t where id=?";
 		Actor actor=this.jdbcTemplate.queryForObject(sql, new RowMapper<Actor>(){
 			@Override
 			public Actor mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -74,7 +74,7 @@ public class DaoDemo extends BaseDao{
 	 */
 	public List queryForList(){
 		//第一种 只有sql的情况下 会返回 List<Map<String,Object>> 会返回字段和 值的键值对 的map类型
-		String sql="select t.* from px_c_message t";
+		String sql="select t.* from sys_user t";
 		List<Map<String,Object>> list=this.jdbcTemplate.queryForList(sql);
 		for(Map<String,Object> m:list){
 			for(String str:m.keySet()){
