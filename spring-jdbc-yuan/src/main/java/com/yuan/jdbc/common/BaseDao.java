a98087a56fb85677c54ae4335d476701ec8452ad
yuanenguang <1534915553@qq.com>
2016-07-04 22:26:48 +0800

    jdbcTemplate

--- spring-jdbc-yuan/src/main/java/com/yuan/jdbc/common/BaseDao.java
+++ spring-jdbc-yuan/src/main/java/com/yuan/jdbc/common/BaseDao.java
@@ -12,6 +12,12 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class BaseDao {
+	/**
+	 * 如果想用 JdbcDaoSupport 则不能用注解的方式自动注入 
+	 * 只能用配置文件的方式 把dao层配置进bean 并注入属性 dataSource
+	 * 原因是 JdbcDaoSupport 中的dataSource不是用的注解的方式自动注入
+	 * 不管是继承JdbcDaoSupport还是如下 最终的目的都是用JdbcTemplate
+	 */
 	protected JdbcTemplate jdbcTemplate;
 	protected DataSource datasource;
 	@Autowired
