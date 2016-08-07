2fd0017747056c276e781354b964c99d15b79353
袁恩光 <1534915553@qq.com>
2016-08-07 08:34:20 +0800

    spring-mvc-withoutxml

---
+++ spring-mvc-withoutxml-yuan/src/main/java/com/yuan/WebConfig.java
@@ -0,0 +1,30 @@
+package com.yuan;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.yuan.config.MvcConfig;
+/**
+ * 通过 webapplicationInitializer 接口代替web.xml 配置
+ * @author yuaneg
+ *
+ */
+public class WebConfig implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(MvcConfig.class);
+		ctx.setServletContext(servletContext);
+		Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
+		servletContext.addListener(new ContextLoaderListener());
+	}
+}
