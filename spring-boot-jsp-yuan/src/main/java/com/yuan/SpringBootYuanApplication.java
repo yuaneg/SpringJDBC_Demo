fc137f6414210ff204151b9620f5b05d7418f779
袁恩光 <1534915553@qq.com>
2016-08-07 16:20:13 +0800

    spring-boot banner和 application.properties的配置

--- spring-boot-jsp-yuan/src/main/java/com/yuan/SpringBootYuanApplication.java
+++ spring-boot-jsp-yuan/src/main/java/com/yuan/SpringBootYuanApplication.java
@@ -3,9 +3,14 @@ package com.yuan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * spring-boot 的核心注解
+ * @author yuaneg
+ *
+ */
 @SpringBootApplication
 public class SpringBootYuanApplication {
-
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootYuanApplication.class, args);
 	}
