aef9a4ef616843442162c843704f23e58282b2ce
袁恩光 <1534915553@qq.com>
2016-08-07 21:36:07 +0800

    thymeleaf

--- spring-boot-thymeleaf-jdbc-yuan/src/main/java/com/yuan/controller/HomeController.java
+++ spring-boot-thymeleaf-jdbc-yuan/src/main/java/com/yuan/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.yuan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String home(){
+	public String home(Model model){
+		model.addAttribute("name", "袁恩光");
 		return "home";
 	}
 }
