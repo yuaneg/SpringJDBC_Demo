85ffedf34edf887377fa069049bc2370720e96e2
袁恩光 <1534915553@qq.com>
2016-08-10 23:03:23 +0800

    用声明式事务

--- spring-boot-thymeleaf-jdbc-yuan/src/main/java/com/yuan/SpringBootThymeleafYuanApplication.java
+++ spring-boot-thymeleaf-jdbc-yuan/src/main/java/com/yuan/SpringBootThymeleafYuanApplication.java
@@ -1,27 +1,13 @@
 package com.yuan;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+//开启事务
 @EnableTransactionManagement
 public class SpringBootThymeleafYuanApplication {
-	
-	/**
-	 * 开启事务支持
-	 * @param dataSource
-	 * @return
-	 */
-	@Bean
-	public PlatformTransactionManager txManager(DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
-	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeleafYuanApplication.class, args);
